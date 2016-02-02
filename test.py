4d4814e8076aa5878ee51ae0f1cdcceec04cc1c5
YogHrt <increasezhg@gmai.com>
2016-02-02 18:40:00 +0800

    'modify'

--- test.py
+++ test.py
@@ -1,4 +1,30 @@
 #-*- coding:utf-8 -*-
+
+#2-3
+print(5//2)
+print(3**2)
+ind2=input("number:")
+print("%d" % int(ind2))
+
+#2-5
+i=0
+while(i<11):
+    i=i+1
+    print(i)
+
+for j in range(11):
+    print(j)
+
+#2-6
+ind3=int(input("num:"))
+if ind3>0:
+    print(">0")
+elif ind3<0:
+    print("<0")
+else:
+    print("=0")
+
+#2-7
 ind=input("input:")
 k=0
 while k<len(ind):
@@ -9,22 +35,9 @@ for o in ind:
     print(o,end='')
 print(end='\n')
 
-print(5//2)
-print(3**2)
-ind2=input("number:")
-print("%d" % int(ind2))
-i=0
-while(i<11):
-    i=i+1
-    print(i)
-
-for j in range(11):
-    print(j)
-
-ind3=int(input("num:"))
-if ind3>0:
-    print(">0")
-elif ind3<0:
-    print("<0")
-else:
-    print("=0")
+#2-9
+ind4=range(5)
+ind5=(2,2,3,4,5)
+f=lambda x:x[0]+x[1]+x[2]+x[3]+x[4]
+print(f(ind4)/5.0)
+print(f(ind5)/5)
